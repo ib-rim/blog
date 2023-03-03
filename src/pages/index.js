c5f5dddde1b924a8dc1e13419ab059f4d3077374
ib-rim <ib.bashir@hotmail.com>
2023-03-03 17:38:14 +0000

    Fixed ESLint errors

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,18 @@ export default function Home() {
         <PageWrapper>
             <main className={styles.main}>
                 <div>
-                    <h1 className={styles.heading}>Ibrahim Bashir's Blog</h1>
-                    <h2 className={styles.subheading}>What's this for?</h2>
+                    <h1 className={styles.heading}>Ibrahim Bashir&apos;s Blog</h1>
+                    <h2 className={styles.subheading}>What&apos;s this for?</h2>
                     <ul className={styles.description}>
-                        <li>Anything I've learnt as I develop my skills and gain experience in Front End Development and UX Design.</li>
-                        <li>Any achievements I'm proud of; both professional and personal.</li>
+                        <li>Anything I&apos;ve learnt as I develop my skills and gain experience in Front End Development and UX Design.</li>
+                        <li>Any achievements I&apos;m proud of; both professional and personal.</li>
                     </ul>
                 </div>
                 <nav>
                     <h2 className={styles.subheading}>Blog Posts</h2>
                     <ul className={styles.posts}>
+                        <li><Link href='/' className={styles.postLink}>The 10 Design Principles of the Government Digital Service</Link></li>
+                        <li><Link href='/' className={styles.postLink}>The 15 Principles of Good Service Design</Link></li>
                     </ul>
                 </nav>
             </main>
