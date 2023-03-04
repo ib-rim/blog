b9993d236af761586a30f311d5ac5adae6ea5085
ib-rim <ib.bashir@hotmail.com>
2023-03-04 16:03:25 +0000

    Fix semantic html

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,8 +6,8 @@ const heebo = Heebo({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }) {
     return (
-        <main className={heebo.className}>
+        <div className={heebo.className}>
             <Component {...pageProps} />
-        </main>
+        </div>
     )
 }
