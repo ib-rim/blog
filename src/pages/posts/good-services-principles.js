d493f9f0ff42a945753abcb17b927f4e28f5c7ac
ib-rim <ib.bashir@hotmail.com>
2023-03-08 16:18:33 +0000

    Started good services principles post

---
+++ src/pages/posts/good-services-principles.js
@@ -0,0 +1,195 @@
+import PageWrapper from '@/components/PageWrapper';
+import styles from '@/styles/GoodServicesPrinciples.module.css'
+import Image from 'next/image';
+
+export default function GoodServicesPrinciples() {
+    return (
+        <PageWrapper title='The 15 Principles of Good Service Design'>
+            <main className={styles.main}>
+                <h1 className={styles.heading}>The 15 Principles of Good Service Design</h1>
+                <div className={styles.principles}>
+                    <section className={styles.principle}>
+                        <div className={styles.principleImage}>
+                            <Image fill src={''} alt="" />
+                        </div>
+                        <div className={styles.principleText}>
+                            <h2 className={styles.principleName}>Be easy to find</h2>
+                            <div className={styles.principleDescription}>
+                                <p>Be easy to find</p>
+                                <p>Be easy to find</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className={styles.principle}>
+                        <div className={styles.principleImage}>
+                            <Image fill src={''} alt="" />
+                        </div>
+                        <div className={styles.principleText}>
+                            <h2 className={styles.principleName}>Clearly explain its purpose</h2>
+                            <div className={styles.principleDescription}>
+                                <p>Clearly explain its purpose</p>
+                                <p>Clearly explain its purpose</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className={styles.principle}>
+                        <div className={styles.principleImage}>
+                            <Image fill src={''} alt="" />
+                        </div>
+                        <div className={styles.principleText}>
+                            <h2 className={styles.principleName}>Set a user&apos;s expectations of the service</h2>
+                            <div className={styles.principleDescription}>
+                                <p>Set a user&apos;s expectations of the service</p>
+                                <p>Set a user&apos;s expectations of the service</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className={styles.principle}>
+                        <div className={styles.principleImage}>
+                            <Image fill src={''} alt="" />
+                        </div>
+                        <div className={styles.principleText}>
+                            <h2 className={styles.principleName}>Enable each user to complete the outcome they set out to do</h2>
+                            <div className={styles.principleDescription}>
+                                <p>Enable each user to complete the outcome they set out to do</p>
+                                <p>Enable each user to complete the outcome they set out to do</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className={styles.principle}>
+                        <div className={styles.principleImage}>
+                            <Image fill src={''} alt="" />
+                        </div>
+                        <div className={styles.principleText}>
+                            <h2 className={styles.principleName}>Work in a way that is familiar</h2>
+                            <div className={styles.principleDescription}>
+                                <p>Work in a way that is familiar</p>
+                                <p>Work in a way that is familiar</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className={styles.principle}>
+                        <div className={styles.principleImage}>
+                            <Image fill src={''} alt="" />
+                        </div>
+                        <div className={styles.principleText}>
+                            <h2 className={styles.principleName}>Require no prior knowledge to use</h2>
+                            <div className={styles.principleDescription}>
+                                <p>Require no prior knowledge to use</p>
+                                <p>Require no prior knowledge to use</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className={styles.principle}>
+                        <div className={styles.principleImage}>
+                            <Image fill src={''} alt="" />
+                        </div>
+                        <div className={styles.principleText}>
+                            <h2 className={styles.principleName}>Be agnostic of organisational structures</h2>
+                            <div className={styles.principleDescription}>
+                                <p>Be agnostic of organisational structures</p>
+                                <p>Be agnostic of organisational structures</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className={styles.principle}>
+                        <div className={styles.principleImage}>
+                            <Image fill src={''} alt="" />
+                        </div>
+                        <div className={styles.principleText}>
+                            <h2 className={styles.principleName}>Require the minimum possible steps to complete</h2>
+                            <div className={styles.principleDescription}>
+                                <p>Require the minimum possible steps to complete</p>
+                                <p>Require the minimum possible steps to complete</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className={styles.principle}>
+                        <div className={styles.principleImage}>
+                            <Image fill src={''} alt="" />
+                        </div>
+                        <div className={styles.principleText}>
+                            <h2 className={styles.principleName}>Be consistent throughout</h2>
+                            <div className={styles.principleDescription}>
+                                <p>Be consistent throughout</p>
+                                <p>Be consistent throughout</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className={styles.principle}>
+                        <div className={styles.principleImage}>
+                            <Image fill src={''} alt="" />
+                        </div>
+                        <div className={styles.principleText}>
+                            <h2 className={styles.principleName}>Have no dead ends</h2>
+                            <div className={styles.principleDescription}>
+                                <p>Have no dead ends</p>
+                                <p>Have no dead ends</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className={styles.principle}>
+                        <div className={styles.principleImage}>
+                            <Image fill src={''} alt="" />
+                        </div>
+                        <div className={styles.principleText}>
+                            <h2 className={styles.principleName}>Be usable by everyone, equally</h2>
+                            <div className={styles.principleDescription}>
+                                <p>Be usable by everyone, equally</p>
+                                <p>Be usable by everyone, equally</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className={styles.principle}>
+                        <div className={styles.principleImage}>
+                            <Image fill src={''} alt="" />
+                        </div>
+                        <div className={styles.principleText}>
+                            <h2 className={styles.principleName}>Encourage the right behaviours from users and service providers</h2>
+                            <div className={styles.principleDescription}>
+                                <p>Encourage the right behaviours from users and service providers</p>
+                                <p>Encourage the right behaviours from users and service providers</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className={styles.principle}>
+                        <div className={styles.principleImage}>
+                            <Image fill src={''} alt="" />
+                        </div>
+                        <div className={styles.principleText}>
+                            <h2 className={styles.principleName}>Quickly respond to change</h2>
+                            <div className={styles.principleDescription}>
+                                <p>Quickly respond to change</p>
+                                <p>Quickly respond to change</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className={styles.principle}>
+                        <div className={styles.principleImage}>
+                            <Image fill src={''} alt="" />
+                        </div>
+                        <div className={styles.principleText}>
+                            <h2 className={styles.principleName}>Clearly explain why a decision has been made</h2>
+                            <div className={styles.principleDescription}>
+                                <p>Clearly explain why a decision has been made</p>
+                                <p>Clearly explain why a decision has been made</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className={styles.principle}>
+                        <div className={styles.principleImage}>
+                            <Image fill src={''} alt="" />
+                        </div>
+                        <div className={styles.principleText}>
+                            <h2 className={styles.principleName}>Make it easy to get human assistance</h2>
+                            <div className={styles.principleDescription}>
+                                <p>Make it easy to get human assistance</p>
+                                <p>Make it easy to get human assistance</p>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </main>
+        </PageWrapper>
+    )
+}
